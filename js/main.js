c6f7ce0bf2b37e49228371aaa27f1f9ececd55a6
Vlad <vlad19943103@gmail.com>
2016-10-31 21:54:42 +0100

    Add Information container

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.getElementById("icon-mont").addEventListener("click", function() {
+    document.getElementById("info-mont").style.visibility="visible";
+}, false);
+
+document.getElementById("icon-close").addEventListener("click",
+  function() {
+    document.getElementById("info-mont").style.visibility="hidden";
+  }, false);
